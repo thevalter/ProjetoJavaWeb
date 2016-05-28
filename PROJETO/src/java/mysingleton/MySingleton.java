401209174f3c75edb11dd966072fbd9244ab4cf1
Douglas Oliveira <douglas.tom@hotmail.com>
2016-05-28 17:42:40 -0300

    Update MySingleton.java

--- PROJETO/src/java/mysingleton/MySingleton.java
+++ PROJETO/src/java/mysingleton/MySingleton.java
@@ -1,48 +1,62 @@
+package Servlets;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mysingleton;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import Bean.Pessoa;
+import DAO.JdbcPessoaDao;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author Douglas
  */
-public class MySingleton {
+@WebServlet(urlPatterns = {"/ServletCadastro"})
+public class ServletCadastro extends HttpServlet {
 
-    private static Connection connection = null;
-    private MySingleton(){}
+
+ 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
     
-    public static Connection getConnection(){
-        if(!(connection instanceof Connection)){
-            try{
-            
-                Class.forName("org.postgresql.Driver");
-                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/AulaSingleton", "postgres", "isabelle");
-                
-            
-            }catch(SQLException | ClassNotFoundException e){
-                e.printStackTrace();
-                 System.out.println("Oops! Ocorreu um erro inesperado!");
-            }
-          
-        }
-    return connection;
+       Pessoa novaPessoa = new Pessoa();
+       novaPessoa.setNome(request.getParameter("nome"));
+       novaPessoa.setCpf(request.getParameter("cpf"));
+       novaPessoa.setUsuario(request.getParameter("usuario"));
+       novaPessoa.setSenha(request.getParameter("senha"));
+       novaPessoa.setRg(request.getParameter("rg"));
+       novaPessoa.setEmail(request.getParameter("email"));
+       novaPessoa.setEndereco(request.getParameter("endereco"));
+        novaPessoa.setTelefone(request.getParameter("telefone"));
+       
+       JdbcPessoaDao pessoa = new JdbcPessoaDao();
+       boolean validarCadastro = pessoa.insert(novaPessoa);
+       int validar = 2;
+       
+       if(validarCadastro){
+        RequestDispatcher r = request.getRequestDispatcher( "exibir.jsp" );
+        request.setAttribute("pessoa", novaPessoa);
+        r.forward( request, response );  
+       }else{
+        RequestDispatcher r = request.getRequestDispatcher( "index.jsp" );
+        request.setAttribute("validar", validar);
+        r.forward( request, response );
+       
+       
+       }
+        
     }
-    
-    public static void closeConnection() throws SQLException{
-        if(connection instanceof Connection){
-            
-                connection.close();
-                
-        }
-    
-    
-    }
-    
+
+
+
 }
